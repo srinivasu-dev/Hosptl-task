febfbb93107df10a0ac01558b32a647f1286ede9
srinivasu-dev <125882738+srinivasu-dev@users.noreply.github.com>
2023-08-29 10:05:54 +0530

    Update sonar.js

--- Hospital-management-nodejs-master/sonar.js
+++ Hospital-management-nodejs-master/sonar.js
@@ -1,9 +1,9 @@
  const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.85.50.213:9000',
+  serverUrl: 'http://52.91.186.130:9000/',
   options: {
-    'sonar.login': '84331cb61b186d1050a627da5da712b1c9c09f69',
+    'sonar.login': '551fe364e574e7b6dec6d5e0f6d561368fc3f251',
     'sonar.sources': './',
     'sonar.exclusions': 'node_modules/**,public/**,views/**,test/**',
     'sonar.projectKey': 'nhs-app',
